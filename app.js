0fc6d5662d84085a0a173bb647a672888f866eaa
franciscorafaelpires <franciscorafaelpires@gmail.com>
2025-06-21 10:58:31 -0300

    ajustes para o vercel2

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 // usando os arquivos estáticos
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 // Rotas para as páginas do portfólio
